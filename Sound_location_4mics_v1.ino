1ab66d0d7cb70abff638f616789dd2441d1e98ab
C3-ZH <93315549+C3-ZH@users.noreply.github.com>
2022-02-17 16:26:03 +0000

    Update Sound_location_4mics_v1.ino
    Added more detailed comments describing what each line of the code is doing. Code optimised by removing oldplusnewsv variable and removing lines of code associated with it (deemed unnecessary).

--- Sound_location_4mics_v1.ino
+++ Sound_location_4mics_v1.ino
@@ -17,11 +17,10 @@ void setup(void)
 void loop(){
  
  int n1;
- for (n1 = 0; n1 <= 3; n1++)
+ for (n1 = 0; n1 <= 3; n1++)   //Execute sound location algorithm n1 times
  {
   float signalvalue0;      //1st signal read variable for 1st mic
   float new_sv0;           //2nd signal read variable
-  float oldplusnew_sv0;    //Sum of the 2 signal values
   float integral_value0;   //Approximate definite integral value for 1 trapezium
   float total_iv0 = 0;     //Total integral value so far
   float t0_0;               //Time of 1st signal read
@@ -30,7 +29,6 @@ void loop(){
   
   float signalvalue1;      //2nd mic variables
   float new_sv1;
-  float oldplusnew_sv1;
   float integral_value1;
   float total_iv1 = 0;
   float t0_1;
@@ -39,7 +37,6 @@ void loop(){
   
   float signalvalue2;     //3rd mic variables
   float new_sv2;
-  float oldplusnew_sv2;
   float integral_value2;
   float total_iv2 = 0;
   float t0_2;
@@ -48,7 +45,6 @@ void loop(){
   
   float signalvalue3;     //4th mic variables
   float new_sv3;
-  float oldplusnew_sv3;
   float integral_value3;
   float total_iv3 = 0;
   float t0_3;
@@ -84,66 +80,62 @@ void loop(){
         signalvalue3 = signalvalue3 * -1;
        }
   
-  for(n2 = 0;n2 <= 500;n2++)
+  for(n2 = 0;n2 <= 500;n2++)                        //Execute sound input processing n2 amount of times
     {                                                  
-     t1_0 = micros();                               //Mic 1
-     new_sv0 = analogRead(A0);
-     if (new_sv0 <= 0)
+     t1_0 = micros();                               //Microprocessor time operating when next signal is read
+     new_sv0 = analogRead(A0);                      //Read analog signal again
+     if (new_sv0 <= 0)                              //If signal value is negative convert to positive
        {
         new_sv0 = new_sv0 * -1;
-       }
-     oldplusnew_sv0 = signalvalue0 + new_sv0;
-     t10_0 = t1_0 - t0_0;
-     integral_value0 = (oldplusnew_sv0 / 2) * t10_0;
-     total_iv0 = total_iv0 + integral_value0;
-     signalvalue0 = new_sv0;
-     t0_0 = t1_0;
+        }
+     t10_0 = t1_0 - t0_0;                           //Find time difference between when the values were read
+     integral_value0 = (signalvalue0 + new_sv0 / 2) * t10_0; //Use trapezium rule to aprroximate part of the definite integral
+     total_iv0 = total_iv0 + integral_value0;        //Add area of this trapezium to total integral value calculated so far
+     signalvalue0 = new_sv0;                         //Overwrite first signal value to the value of the second signal read
+     t0_0 = t1_0;                                    //Overwrite first time to the value of the second time
 
-     t1_1 = micros();                              //Mic 2
+     t1_1 = micros();                              //Repeat for Mic 2
      new_sv1 = analogRead(A1);
      if (new_sv1 <= 0)
        {
         new_sv1 = new_sv1 * -1;
        }
-     oldplusnew_sv1 = signalvalue1 + new_sv1;
      t10_1 = t1_1 - t0_1;
-     integral_value1 = (oldplusnew_sv1 / 2) * t10_1;
+     integral_value1 = (signalvalue1 + new_sv1 / 2) * t10_1;
      total_iv1 = total_iv1 + integral_value1;
      signalvalue1 = new_sv1;
      t0_1 = t1_1;
 
-     t1_2 = micros();                              //Mic 3
+     t1_2 = micros();                              // Reapeat for Mic 3
      new_sv2 = analogRead(A2);
      if (new_sv2 <= 0)
        {
         new_sv2 = new_sv2 * -1;
        }
-     oldplusnew_sv2 = signalvalue2 + new_sv2;
      t10_2 = t1_2 - t0_2;
-     integral_value2 = (oldplusnew_sv2 / 2) * t10_2;
+     integral_value2 = (signalvalue2 + new_sv2 / 2) * t10_2;
      total_iv2 = total_iv2 + integral_value2;
      signalvalue2 = new_sv2;
      t0_2 = t1_2;
 
-     t1_3 = micros();                              //Mic 4
+     t1_3 = micros();                              //Repeat for Mic 4
      new_sv3 = analogRead(A3);
      if (new_sv3 <= 0)
        {
         new_sv3 = new_sv3 * -1;
        }
-     oldplusnew_sv3 = signalvalue3 + new_sv3;
      t10_3 = t1_3 - t0_3;
-     integral_value3 = (oldplusnew_sv3 / 2) * t10_3;
+     integral_value3 = (signalvalue3 + new_sv3 / 2) * t10_3;
      total_iv3 = total_iv3 + integral_value3;
      signalvalue3 = new_sv3;
      t0_3 = t1_3;
      
-     delay(10);
+     delay(1);
      }
   
-  if (integral_value0 <= integral_value1 && integral_value0 <= integral_value2 && integral_value0 <= integral_value3)
+  if (integral_value0 <= integral_value1 && integral_value0 <= integral_value2 && integral_value0 <= integral_value3) //If mic 1 detects a louder sound
   {
-      analogWrite (E1, 255);        //Move forward if mic0 has highest integral value
+      analogWrite (E1, 255);        //Move forward
       digitalWrite(M1, 0);
       analogWrite (E2, 255);
       digitalWrite(M2, 0);
@@ -153,9 +145,9 @@ void loop(){
       analogWrite (E2, 0);
       digitalWrite(M2, 0);
   }
-  else if (integral_value1 <= integral_value0 && integral_value1 <= integral_value2 && integral_value1 <= integral_value3)
+  else if (integral_value1 <= integral_value0 && integral_value1 <= integral_value2 && integral_value1 <= integral_value3) //If mic 2 detects a louder sound
   {
-      analogWrite (E1, 255);        //Rotate right 90 degrees and then move forward if mic1 has highest integral value
+      analogWrite (E1, 255);        //Rotate right 90 degrees and then move forward
       digitalWrite(M1, 1);
       analogWrite (E2, 255);
       digitalWrite(M2, 0);
@@ -170,9 +162,9 @@ void loop(){
       analogWrite (E2, 0);
       digitalWrite(M2, 0);
   }
-  else if (integral_value2 <= integral_value0 && integral_value2 <= integral_value1 && integral_value2 <= integral_value3)
+  else if (integral_value2 <= integral_value0 && integral_value2 <= integral_value1 && integral_value2 <= integral_value3) //If mic 3 detects a louder sound
   {
-      analogWrite (E1, 255);        //Rotate right 180 degrees and then move forward if mic2 has highest integral value
+      analogWrite (E1, 255);        //Rotate right 180 degrees and then move forward
       digitalWrite(M1, 1);
       analogWrite (E2, 255);
       digitalWrite(M2, 0);
@@ -187,9 +179,9 @@ void loop(){
       analogWrite (E2, 0);
       digitalWrite(M2, 0);
   }
-  else if(integral_value3 <= integral_value0 && integral_value3 <= integral_value1 && integral_value3 <= integral_value2)
+  else if(integral_value3 <= integral_value0 && integral_value3 <= integral_value1 && integral_value3 <= integral_value2) //If mic 4 detects a louder sound
   {
-      analogWrite (E1, 255);        //Rotate left 90 degrees and then move forward if mic3 has highest integral value
+      analogWrite (E1, 255);        //Rotate left 90 degrees and then move forward
       digitalWrite(M1, 0);
       analogWrite (E2, 255);
       digitalWrite(M2, 1);
