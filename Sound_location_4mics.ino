a30c0f62e0c6def759ab7ecaa5d99274794fc37b
C3-ZH <93315549+C3-ZH@users.noreply.github.com>
2022-03-15 20:09:50 +0000

    Update Sound_location_4mics.ino
    Optimised code flow for more accurate readings between mics.

--- Sound_location_4mics.ino
+++ Sound_location_4mics.ino
@@ -16,8 +16,8 @@ void setup(void)
 
 void loop(){
  
- int n1;
- for (n1 = 0; n1 <= 3; n1++)   //Execute sound location algorithm n1 times
+ int move_executions;
+ for (move_executions = 0; move_executions <= 10; move_executions++)   //Execute sound location algorithm x times
  {
   float signalvalue0;      //1st signal read variable for 1st mic
   float new_sv0;           //2nd signal read variable
@@ -50,48 +50,51 @@ void loop(){
   float t0_3;
   float t1_3;
   float t10_3;
-  int n2;
+  int sample_no;
 
   t0_0 = micros();                             //Record time microprocessor has been operating when first signal is read
   signalvalue0 = analogRead(A0);              //Read signal from mic circuit
+  
+  t0_1 = micros();
+  signalvalue1 = analogRead(A1);
+  
+  t0_2 = micros();
+  signalvalue2 = analogRead(A2);
+  
+  t0_3 = micros();
+  signalvalue3 = analogRead(A3);
+   
   signalvalue0 = signalvalue0 - 70;           //Account for offset in read signal value
   if (signalvalue0 <= 0)                      
        {                                        //Convert signal value to positive if negative
         signalvalue0 = signalvalue0 * -1;
        }
   
-  t0_1 = micros();
-  signalvalue1 = analogRead(A1);
   signalvalue1 = signalvalue1 - 522.5;
   if (signalvalue1 <= 0)
        {
         signalvalue1 = signalvalue1 * -1;
        }
   
-  t0_2 = micros();
-  signalvalue2 = analogRead(A2);
   signalvalue2 = signalvalue2 - 519;
   if (signalvalue2 <= 0)
        {
         signalvalue2 = signalvalue2 * -1;
        }
-  
-  t0_3 = micros();
-  signalvalue3 = analogRead(A3);
+   
   signalvalue3 = signalvalue3 - 532;
   if (signalvalue3 <= 0)
        {
         signalvalue3 = signalvalue3 * -1;
        }
   
-  for(n2 = 0;n2 <= 10000;n2++)                        //Execute sound input processing n2 amount of times
+  for(sample_no = 0; sample_no <= 10000; sample_no++)      //Execute sound input processing x amount of times
     {                                                  
      t1_0 = micros();                               //Microprocessor time operating when next signal is read
      new_sv0 = analogRead(A0);                      //Read analog signal again
 
      t1_1 = micros();                              //Repeat for Mic 2
      new_sv1 = analogRead(A1);
-
 
      t1_2 = micros();                              // Reapeat for Mic 3
      new_sv2 = analogRead(A2);
